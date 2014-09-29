e3056c4bb76c46f00d90812a314b825a2b70553b
Ashish Kedia <ashish1294@gmail.com>
2014-09-29 10:27:53 +0530

    A sample code added

---
+++ blas_axpy.cpp
@@ -0,0 +1,36 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<iostream>
+#include<queue>
+#include<utility>
+#include<cblas.h>
+//#include<common.h>
+
+using namespace std;
+
+int main()
+{
+	blasint n = 10;
+    blasint in_x =1;
+    blasint in_y =1;
+
+    std::vector<double> x(n);
+    std::vector<double> y(n);
+
+    double alpha = 10;
+
+    std::fill(x.begin(),x.end(),1.0);
+    std::fill(y.begin(),y.end(),2.0);
+
+    cblas_daxpy( n, alpha, &x[0], in_x, &y[0], in_y);
+
+    //Print y
+    for(int j=0;j<n;j++)
+        std::cout << y[j] << "\t";
+
+    std::cout << std::endl;
+}
